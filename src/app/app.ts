66171a7ae519dd6166383acd1465382e159cba08
Petyaev Bair <petyaev01@mail.ru>
2026-03-01 15:00:01 +0300

    +1 коммит

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('filmlvddfvy');
+  protected readonly title = signal('filmly');
 }
